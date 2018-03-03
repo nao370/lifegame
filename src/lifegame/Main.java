dca8e10dc2a56f7226d1075096ff08de8b554f92
nao370 <kilabasketball@yahoo.co.jp>
2018-03-04 01:32:04 +0900

    add MainClass
    Mainクラスを作ってウインドウ呼び出しまで完了した。

---
+++ src/lifegame/Main.java
@@ -0,0 +1,21 @@
+package lifegame;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public Main() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		JFrame mainFrame = new JFrame("サンプル");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1000, 800);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+	}
+
+}
